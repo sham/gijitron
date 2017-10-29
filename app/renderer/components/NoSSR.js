21edcc6f2872ae529e5a35e9b147f4bf15d29a71
しゃむ <shamofu@gmail.com>
2017-10-29 16:39:16 +0900

    Move NoSSR class to components

---
+++ app/renderer/components/NoSSR.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class NoSSR extends React.Component {
+  constructor(...args) {
+    super(...args);
+    this.state = {
+      canRender: false
+    };
+  }
+  
+  componentDidMount() {
+    this.setState({ canRender: true });
+  }
+  
+  render() {
+    const DefaultOnSSR = () => (<span></span>);
+    const { children, onSSR = <DefaultOnSSR /> } = this.props;
+    const { canRender } = this.state;
+  
+    return canRender ? children : onSSR;
+  }
+}
+NoSSR.propTypes = {
+  children: PropTypes.element.isRequired,
+  onSSR: PropTypes.element
+};
