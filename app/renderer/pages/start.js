e61203174a84bc90010aadd37162072279ecaebf
しゃむ <shamofu@gmail.com>
2017-11-28 12:21:51 +0900

    Minor fix

--- app/renderer/pages/start.js
+++ app/renderer/pages/start.js
@@ -6,18 +6,16 @@ import NoSSR from '../components/NoSSR';
 import stylesheet from '../styles/start.css';
 
 
-export default class extends React.Component {
-  render() {
-    const disableDnD = 'document.ondragover = document.ondrop = function(e) { e.preventDefault() }';
-    return (<div id='container'>
-      <Head>
-        <title>gijitron</title>
-        <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
-        <script dangerouslySetInnerHTML={{ __html: disableDnD }} />
-      </Head>
-      <NoSSR>
-        <GijirokuMaker />
-      </NoSSR>
-    </div>);
-  }
-}
+export default () => {
+  const disableDnD = 'document.ondragover = document.ondrop = function(e) { e.preventDefault() }';
+  return (<div id='container'>
+    <Head>
+      <title>gijitron</title>
+      <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
+      <script dangerouslySetInnerHTML={{ __html: disableDnD }} />
+    </Head>
+    <NoSSR>
+      <GijirokuMaker />
+    </NoSSR>
+  </div>);
+};
