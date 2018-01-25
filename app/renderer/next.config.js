3bf20d5e607fb442cbce2b7f680f5191581fb106
しゃむ <shamofu@gmail.com>
2018-01-26 01:33:13 +0900

    Use styled-jsx

--- app/renderer/next.config.js
+++ app/renderer/next.config.js
@@ -1,43 +1,7 @@
-const path = require('path');
-const glob = require('glob');
+exports.webpack = (config) => Object.assign(config, {
+  target: 'electron-renderer'
+});
 
-module.exports = {
-  webpack(config) {
-    config.target = 'electron-renderer';
-    config.module.rules.push(
-      {
-        test: /\.s?css$/,
-        loader: 'emit-file-loader',
-        options: {
-          name: 'dist/[path][name].[ext]'
-        }
-      },
-      {
-        test: /\.css$/,
-        use: ['babel-loader', 'raw-loader', 'postcss-loader']
-      },
-      {
-        test: /\.s(a|c)ss$/,
-        use: ['babel-loader', 'raw-loader', 'postcss-loader',
-          { loader: 'sass-loader',
-            options: {
-              includePaths: ['styles', 'node_modules']
-                .map((d) => path.join(__dirname, d))
-                .map((g) => glob.sync(g))
-                .reduce((a, c) => a.concat(c), [])
-            }
-          }
-        ]
-      }
-    );
-    return config;
-  },
-  exportPathMap() {
-    // Let Next.js know where to find the entry page
-    // when it's exporting the static bundle for the use
-    // in the production version of your app
-    return {
-      '/start': { page: '/start' }
-    };
-  }
-};
+exports.exportPathMap = () => ({
+  '/start': { page: '/start' }
+});
